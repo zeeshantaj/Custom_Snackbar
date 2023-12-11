6329d85f97efb4ddd3d2c4fd9cd28f1b8178c777
zeeshantaj <91092210+zeeshantaj@users.noreply.github.com>
2023-12-11 15:05:17 +0500

    Create Snackbar.java

---
+++ Snackbar.java
@@ -0,0 +1,28 @@
+  Snackbar snackbar = Snackbar.make(constraintLayout, "", Snackbar.LENGTH_INDEFINITE);
+                    View customSnackbarView = getLayoutInflater().inflate(R.layout.custom_snake_bar, null);
+                    snackbar.getView().setBackgroundColor(Color.TRANSPARENT); // Make Snackbar background transparent
+                    @SuppressLint("RestrictedApi") Snackbar.SnackbarLayout snackbarLayout = (Snackbar.SnackbarLayout) snackbar.getView();
+                    snackbarLayout.addView(customSnackbarView, 0);
+
+// Find the buttons in the custom layout
+                    Button btnView = customSnackbarView.findViewById(R.id.btnView);
+                    Button btnDismiss = customSnackbarView.findViewById(R.id.btnDismiss);
+
+// Set click listeners for the buttons
+                    btnView.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            startActivity(new Intent(Booking_Activity.this, Profile_Activity.class));
+                            snackbar.dismiss(); // Dismiss the snackbar if needed
+                        }
+                    });
+
+                    btnDismiss.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            snackbar.dismiss(); // Dismiss the snackbar if needed
+                            // Your negative action logic here
+                        }
+                    });
+
+                    snackbar.show();
